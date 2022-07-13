5c9e0717d8ac31dba9809809b3e026fd48a11a22
Stanley Lo <stanleyjhl229@gmail.com>
2022-07-12 21:59:27 -0400

    working on PyTorch models

--- da_for_polymers/ML_models/pytorch/NN/nn_regression.py
+++ da_for_polymers/ML_models/pytorch/NN/nn_regression.py
@@ -1,7 +1,21 @@
+from typing import OrderedDict
+import torch
 from torch import nn
+import numpy as np
 import tensorboard
 import torch.nn.functional as F
 
+class OrthoLinear(torch.nn.Linear):
+    def reset_parameters(self):
+        torch.nn.init.orthogonal_(self.weight)
+        if self.bias is not None:
+            torch.nn.init.zeros_(self.bias)
+
+class XavierLinear(torch.nn.Linear):
+    def reset_parameters(self):
+        torch.nn.init.xavier_normal_(self.weight)
+        if self.bias is not None:
+            torch.nn.init.zeros_(self.bias)
 
 class NNModel(nn.Module):
     def __init__(self, config):
@@ -11,17 +25,26 @@ class NNModel(nn.Module):
             config (args): Model Configuration parameters.
         """
         super().__init__()
-        self.linearlayers = nn.Sequential(
+        self.embeds: nn.Sequential = nn.Sequential(
             nn.Embedding(config["vocab_length"], config.embedding_size),
             nn.ReLU(),
-            nn.Linear(config.embedding_size, config.hidden_size),
+            OrthoLinear(config.embedding_size, config.hidden_size),
             nn.ReLU(),
-            nn.Linear(config.hidden_size, config.output_size),
         )
-        self.loss = nn.MSELoss()
+        self.linearlayers: nn.Sequential = nn.Sequential(
+            OrderedDict([OrthoLinear(config.hidden_size, config.hidden_size), nn.ReLU() for _ in range(config["n_layers"])])
+        )
+        self.output: OrthoLinear = OrthoLinear(config.hidden_size, config.output_size)
 
-    def forward(self, x):
-        output = self.linearlayers(x)
-        output = F.relu(output)
+    def forward(self, x: torch.tensor):
+        """
+        Args:
+            x (torch.tensor): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        embeds: torch.tensor = self.embeds(x)
+        linear: torch.tensor = self.linearlayers(embeds)
+        output: torch.tensor = self.output(linear)
         return output
-
--- da_for_polymers/ML_models/pytorch/train.py
+++ da_for_polymers/ML_models/pytorch/train.py
@@ -1,14 +1,169 @@
 import argparse
 import copy
-import tensorboard
-import torch
-import torch.nn as nn
-from torch.optim import Optimizer
-from torch.optim.lr_scheduler import _LRScheduler
+import enum
+import json
+from pyparsing import Opt
 
-# from .data.data import
+# PyTorch TensorBoard support
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+
+from torch import nn
+import pandas as pd
 from torch.utils.data import DataLoader
+from torch import optim
+from torch.optim.lr_scheduler import _LRScheduler
+from da_for_polymers.ML_models.pytorch.pipeline import (
+    process_features,
+    process_target,
+)
+
+from .NN.nn_regression import NNModel
+from .data.data_utils import PolymerDataset
 
 
-def train(model: nn.Module):
-    pass
+def main(config: dict):
+    """
+    Starting from user-specified inputs, a PyTorch model is trained and evaluated.
+
+    Args:
+        config (dict): _description_
+    """
+    # process model configurations
+    with open(config["model_config_path"]) as model_config_path:
+        model_config: dict = json.load(model_config_path)
+    for param in model_config.keys():
+        config[param] = model_config[param]
+
+    # process multiple data files
+    train_paths: str = config["train_paths"]
+    validation_paths: str = config["validation_paths"]
+
+    # if multiple train and validation paths, X-Fold Cross-Validation occurs here.
+    fold: int = 0
+    outer_r: list = []
+    outer_r2: list = []
+    outer_rmse: list = []
+    outer_mae: list = []
+    progress_dict: dict = {"fold": [], "r": [], "r2": [], "rmse": [], "mae": []}
+    for train_path, validation_path in zip(train_paths, validation_paths):
+        train_df: pd.DataFrame = pd.read_csv(train_path)
+        val_df: pd.DataFrame = pd.read_csv(validation_path)
+        # process SMILES vs. Fragments vs. Fingerprints. How to handle that? handle this and tokenization in pipeline
+        (
+            input_train_array,
+            input_val_array,
+        ) = process_features(  # additional features are added at the end of array
+            train_df[config["feature_names"].split(",")],
+            val_df[config["feature_names"].split(",")],
+        )
+        # process target values
+        target_df_columns = config["target_name"].split(",")
+        target_df_columns.extend(config["feature_names"].split(","))
+        (
+            target_train_array,
+            target_val_array,
+            target_max,
+            target_min,
+        ) = process_target(
+            train_df[target_df_columns],
+            val_df[target_df_columns],
+        )
+
+        # Create PyTorch Dataset and DataLoader
+        train_set = PolymerDataset(
+            input_train_array, target_train_array, config["random_state"]
+        )
+        valid_set = PolymerDataset(
+            input_val_array, target_val_array, config["random_state"]
+        )
+        train_dataloader = DataLoader(train_set, batch_size=config["train_batch_size"])
+        valid_dataloader = DataLoader(valid_set, batch_size=config["valid_batch_size"])
+
+        # Choose PyTorch Model
+        if config["model_type"] == "NN":
+            model = NNModel(config)
+        elif config["model_type"] == "LSTM":
+            pass
+
+        # Choose Loss Function
+        if config["loss"] == "MSE":
+            loss_fn = nn.MSELoss()
+        elif config["loss"] == "CrossEntropy":
+            loss_fn = nn.CrossEntropyLoss()
+
+        # Choose PyTorch Optimizer
+        if config["optimizer"] == "Adam":
+            optimizer = optim.Adam(
+                model.parameters(),
+                lr=model_config["lr"],
+                momentum=model_config["momentum"],
+            )
+
+        # TODO: SummaryWriter, Logger, _LRScheduler
+
+        # TRAINING LOOP
+        ## PRESET FUNCTIONS
+        model.train()
+
+        ## LOOP
+        for i, data in enumerate(train_dataloader):
+            inputs, targets = data
+            inputs, targets = inputs.cuda(), targets.cuda()
+            # Zero your gradients for every batch!
+            optimizer.zero_grad()
+            # Make predictions for this batch
+            outputs = model(inputs)
+            # Compute the loss and its gradients
+            loss = loss_fn(outputs, targets)
+            loss.backward()
+            # Adjust learning weights
+            optimizer.step()
+            # Gather data and report
+            running_loss += loss.item()
+
+            # TODO: Gather data and report based on epoch, and report number of examples trained on.
+            # Stop training after max iterations
+
+        # TODO: VALIDATION LOOP
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--train_paths",
+        type=str,
+        nargs="+",
+        help="Path to training data. If multiple training data: format is 'train_0.csv, train_1.csv, train_2.csv', required that multiple validation paths are provided.",
+    )
+    parser.add_argument(
+        "--validation_paths",
+        type=str,
+        nargs="+",
+        help="Path to validation data. If multiple validation data: format is 'val_0.csv, val_1.csv, val_2.csv', required that multiple training paths are provided.",
+    )
+    parser.add_argument(
+        "--feature_names",
+        type=str,
+        help="Choose input features. Format is: ex. SMILES, T_K, P_Mpa - Always put representation at the front.",
+    )
+    parser.add_argument(
+        "--target_name",
+        type=str,
+        help="Choose target values. Format is ex. a_separation_factor",
+    )
+    parser.add_argument("--model_type", type=str, help="Choose model type. (NN, LSTM)")
+    parser.add_argument(
+        "--model_config_path", type=str, help="Filepath of model config JSON"
+    )
+    parser.add_argument(
+        "--results_path",
+        type=str,
+        help="Filepath to location of result summaries and predictions up to the dataset is sufficient.",
+    )
+    parser.add_argument(
+        "--random_state", type=int, default=22, help="Integer number for random seed."
+    )
+    args = parser.parse_args()
+    config = vars(args)
+    main(config)
